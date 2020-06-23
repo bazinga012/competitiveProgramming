7453619cd3d690f5d61b206b8b0c5b7362c04e52
vishalagrawal <vishal.agrawal@docon.co.in>
2020-06-23 22:25:58 +0530

    cpp

--- c_cpp/queues_and_stacks.cpp
+++ c_cpp/queues_and_stacks.cpp
@@ -1,25 +1,30 @@
 #include <queue>
 #include <deque>
+#include <list>
 #include <stack>
+using namespace std;
+int main()
+{
+    queue<int, std::list<int>> Q;
+    Q.push(1);
+    Q.empty(); //check if Q is empty
+    Q.front();
+    Q.pop(); // pop from front
+    Q.push(4);
+    Q.back();
 
-queue<int> Q;
-Q.push(1);
-Q.empty(); //check if Q is empty
-Q.front();
-Q.pop(); // pop from front
-Q.push(4);
-Q.back();
+    deque<int> dQ;
+    dQ.push_back(5);
+    dQ.front();
+    dQ.back();
+    dQ.push_front(5);
+    dQ.pop_front();
+    dQ.pop_back();
 
-deque<int> dQ;
-dQ.push_back(5);
-dQ.front();
-dQ.back();
-dQ.push_front(5);
-dQ.pop_front();
-dQ.pop_back();
-
-stack<int> S;
-S.push(4);
-S.top();
-S.empty();
-S.pop();
+    stack<int> S;
+    S.push(4);
+    S.top();
+    S.empty();
+    S.pop();
+    return 0;
+}
