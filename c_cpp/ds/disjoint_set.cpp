c4435fa3741ab36a99a0001abf7ad9d9a92c1ef0
vishalagrawal <vishal.agrawal@docon.co.in>
2020-06-26 19:34:32 +0530

    disjoint set

--- c_cpp/ds/disjoint_set.cpp
+++ c_cpp/ds/disjoint_set.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <utility>
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<vii> graph;
 using namespace std;
 /* https://www.youtube.com/watch?v=wU6udHRIkcc */
 
-int main(){
+int find(int *parent, int n, int i)
+{
+    if (parent[i] != i)
+        parent[i] = find(parent, n, parent[i]);
+    return parent[i];
+}
+
+
+void unify(int *parent, int *rank, int n, int x, int y)
+{
+    /* Also, size (in place of height) of trees can also be used as rank. 
+    Using size as rank also yields worst case time complexity as O(Logn) */
+    int xroot = find(parent, n, x);
+    int yroot = find(parent, n, y);
+    if (rank[xroot] < rank[yroot])
+    {
+        parent[xroot] = yroot;
+        rank[yroot] += rank[xroot];
+    }
+    else
+    {
+        parent[yroot] = xroot;
+        rank[xroot] += rank[yroot];
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int parent[n];
+    int rank[n];
+    graph G;
+    memset(parent, -1, sizeof(int) * n);
+    memset(rank, 1, sizeof(int) * n);
 
     return 0;
 }
-
