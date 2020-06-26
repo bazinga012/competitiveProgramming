8e3176457d4b70690c91f70ad2968a2e76bcf73e
vishalagrawal <vishal.agrawal@docon.co.in>
2020-06-26 17:14:01 +0530

    heap cpp

--- c_cpp/ds/heap_and_priority_queues.cpp
+++ c_cpp/ds/heap_and_priority_queues.cpp
@@ -1,6 +1,11 @@
 #include <queue>
 #include <vector>
+#include <iostream>
 #include <utility> //pair
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+using namespace std;
 class prioritize
 {
 public:
@@ -18,18 +23,20 @@ public:
     }
 };
 
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    priority_queue<ii> Q1;                   //max heap  in pair accor to first value in case of clash last added
+    priority_queue<ii, vii, greater<ii>> Q2; //min heap in pair accor to first value in case of clash first added
 
-priority_queue<ii> Q1;                   //max heap  in pair accor to first value in case of clash last added
-priority_queue<ii, vii, greater<ii>> Q2; //min heap in pair accor to first value in case of clash first added
+    priority_queue<int> q;                            //max heap  in case of clash last added
+    priority_queue<int, vector<int>, greater<int>> q; //min heap  in case of clash last added
 
-priority_queue<int> q;                            //max heap  in case of clash last added
-priority_queue<int, vector<int>, greater<int>> q; //max heap  in case of clash last added
+    priority_queue<ii, vii, prioritize> Q3;  //max heap in pair accor to second value in case of clash last added
+    priority_queue<ii, vii, prioritize1> Q4; //min heap in pair accor to second value in case of clash first added
 
-priority_queue<ii, vii, prioritize> Q3;  //max heap in pair accor to second value in case of clash last added
-priority_queue<ii, vii, prioritize1> Q4; //min heap in pair accor to second value in case of clash first added
-
-Q1.top();
-Q1.pop();
-Q1.empty();
+    Q1.top();
+    Q1.pop();
+    Q1.empty();
+    return 0;
+}
